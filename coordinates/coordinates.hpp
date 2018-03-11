40ece474156854cc83588f499f6ce054a4062a9e
Almudena Garcia <liberamenso10000@gmail.com>
2018-03-11 22:57:15 +0100

    Update coordinates.hpp

--- coordinates/coordinates.hpp
+++ coordinates/coordinates.hpp
@@ -1,7 +1,7 @@
 /*
 Copyright 2011 Ángel Olivera Crego
 Copyright 2013 Cristian Corado Cordero
-Copyright 2017 Alejandro Domínguez Muñoz
+Copyright 2017 2018 Alejandro Domínguez Muñoz
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
